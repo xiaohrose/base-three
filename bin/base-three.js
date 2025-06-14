43aa785dacdbe2b52d1c0bf1ac1301bfa251c070
xiaohorse <1778911348@qq.com>
2025-06-14 13:59:59 +0800

    feat: 修改包名字

--- bin/base-three.js
+++ bin/base-three.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-require("../dist/index.js");
+const main = require("../dist/index.js");
+
+if (typeof main.default === "function") {
+    main.default(process.argv.slice(2));
+} else {
+    console.error("Error: The module does not export a function.");
+    process.exit(1);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import * as fs from 'fs';
 import * as path from 'path';
+// Function to parse command-line arguments
+function parseCommandLineArgs() {
+    const args = process.argv.slice(2);
+    const options: { [key: string]: string } = {};
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith('--')) {
+            const key = args[i].substring(2);
+            const value = args[i + 1] && !args[i + 1].startsWith('--') ? args[i + 1] : 'true';
+            options[key] = value;
+            if (value !== 'true') i++; // Skip the value
+        }
+    }
+
+    return options;
+}
+
+// Example usage of the function
+const cliArgs = parseCommandLineArgs();
 
 const sourceDir = path.resolve(__dirname, '../template');
 const targetDir = path.resolve(process.cwd());
 
 // Function to copy files and directories recursively
-function copyFolderSync(source: string, target: string) {
+function copyFolderSync(source: string, target: string, pn: string) {
     if (!fs.existsSync(source)) {
         console.error(`Source directory "${source}" does not exist.`);
         return;
@@ -21,14 +40,29 @@ function copyFolderSync(source: string, target: string) {
         const targetPath = path.join(target, item);
 
         if (fs.lstatSync(sourcePath).isDirectory()) {
-            copyFolderSync(sourcePath, targetPath);
+            copyFolderSync(sourcePath, targetPath, pn);
         } else {
-            fs.copyFileSync(sourcePath, targetPath);
+            if (item === 'package.json') {
+                const packageJson = JSON.parse(fs.readFileSync(sourcePath, 'utf-8'));
+                packageJson.name = pn;
+                fs.writeFileSync(targetPath, JSON.stringify(packageJson, null, 2), 'utf-8');
+            } else {
+                fs.copyFileSync(sourcePath, targetPath);
+            }
         }
     }
 }
 
-// Copy the template folder to the current directory
-copyFolderSync(sourceDir, targetDir);
 
-console.log('Template folder copied successfully.');
+
+export default (args: any[]) => {
+    let packageName: string = 'threejs-demo';
+
+    if (args.length > 0) {
+        packageName = args[0]
+    }
+
+    // Copy the template folder to the current directory
+    copyFolderSync(sourceDir, targetDir, packageName);
+    console.log('Template folder copied successfully.');
+}
